a83202b2012204611d57a2d4335f383fea8d44db
rabbunijava <rabbunijava@gmail.com>
2022-07-08 14:07:08 +0530

    Sample-commit

---
+++ Documents/rabbuni/MessageRunnerNew/src/main/java/com/marlabs/caylax/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.marlabs.caylax;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("This is Rabbuni");
+
+	}
+
+}
